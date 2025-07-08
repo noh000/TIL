7d18a537df946f714f0470dab0ff654030b51fb5
noh000 <ay.noh89@gmail.com>
2025-07-08 19:55:14 +0900

    SQL_day7

--- SQL/28-any-all.sql
+++ SQL/28-any-all.sql
@@ -1,23 +1,22 @@
--- any
+USE lecture;
 
--- ANY - 여러 값들 중에 하나라도 조건을 만족하면 TRUE (OR)
-
--- 1. VIP 고객들의 최소 주문액 보다 높은 모든 주문
-
+-- ANY 서브쿼리 결과 중 하나라도 조건을 만족하면 TRUE, 비교 연산자(=<>) 필요
+-- cf) IN 특정 값이 목록 안에 있는지 (리스트/서브쿼리)
+ 
+-- 1. VIP 고객들의 최소 주문액보다 높은 모든 주문
 SELECT s.total_amount
   FROM sales s
   INNER JOIN customers c on s.customer_id = c.customer_id
   WHERE c.customer_type = 'VIP';
 
-
 SELECT
   customer_id,
   product_name,
   total_amount,
-  '일반 고객이지만 VIP 최소보다 높음' AS 구분
+  'VIP보다 구매금액 높은 일반고객' AS 구분
 FROM sales
-WHERE total_amount > ANY(  -- a, b, c, d, e
- -- VIP 들의 모든 주문 금액들 (Vector)
+WHERE total_amount >= ANY(
+  -- VIP들의 모든 주문 금액들 (Vector)
   SELECT s.total_amount
   FROM sales s
   INNER JOIN customers c on s.customer_id = c.customer_id
@@ -25,32 +24,28 @@ WHERE total_amount > ANY(  -- a, b, c, d, e
 ) AND
 customer_id NOT IN (SELECT customer_id FROM customers WHERE customer_type = 'VIP')
 ORDER BY total_amount DESC;
--- 위의 코드는 아래와 같음
+
+-- 어떤 지역 평균 매출액보다라도 높은 주문들
 SELECT
-  customer_id,
+  region,
   product_name,
-  total_amount,
-  '일반 고객이지만 VIP 최소보다 높음' AS 구분
-FROM sales
-WHERE total_amount > (
-  SELECT MIN(s.total_amount) FROM sales s
-  INNER JOIN customers c on s.customer_id = c.customer_id
-  WHERE c.customer_type = 'VIP'
-) AND
-customer_id NOT IN (SELECT customer_id FROM customers WHERE customer_type = 'VIP')
+  total_amount
+FROM sales s
+WHERE total_amount >= ANY( 
+  SELECT AVG(total_amount)
+  FROM sales
+  GROUP BY region
+)
 ORDER BY total_amount DESC;
 
--- 어떤 지역 평균 매출액보다라도 높은 주문들.
-
-SELECT
-  id,
-  customer_id
-FROM sales
-WHERE total_amount > ANY(
-  SELECT avg(total_amount)
-  FROM sales
-  GROUP BY region);
-  
-  
-  -- all
-  
+-- ALL 서브쿼리 결과 모두가 만족해야 TRUE
+-- 모든 부서의 평균연봉보다 더 높은 연봉을 받는 사람 (예시 - 실행 안됨)
+SELECT * 
+FROM employees
+-- 서브쿼리 벡터의 모든 값보다 커야함
+WHERE salary > ALL(
+    -- 각 부서의 평균 연봉 Vector
+    SELECT AVG(salary)
+    FROM employees
+    GROUP BY department_id
+);
--- SQL/29-exists.sql
+++ SQL/29-exists.sql
@@ -1,10 +1,49 @@
-USE lecture;
+-- EXISTS 서브쿼리 결과가 한 줄이라도 **존재**하면 TRUE
+-- WHERE EXISTS (SELECT 1 ... 의 형태로 주로 사용됨
 
--- 한 적 있는
-
+-- 전자제품을 구매한 고객 정보
 SELECT
-  c. customer_name
-FROM customers c
-WHERE EXISTS(
-  SELECT 1 FROM sales s WHERE s.category= '전자제품' AND s.category='의류' AND s.total_amount>=5*power(10, 5)
+  customer_id, customer_name, customer_type
+FROM customers
+WHERE customer_id IN (  -- 결과 리스트를 메모리에 생성, 시간/공간적으로 비효율
+  SELECT customer_id FROM sales WHERE category='전자제품'
 );
+
+-- EXISTS 사용, 같은 결과
+SELECT
+  customer_id, customer_name, customer_type
+FROM customers c
+-- 고객이 전자제품을 구매한 기록이 하나라도 있으면 해당 고객을 포함
+WHERE EXISTS (
+  SELECT 1 FROM sales s
+  WHERE s.customer_id=c.customer_id AND s.category='전자제품'  -- JOIN 없이도 외부값 참조 가능
+);
+
+
+-- 전자제품과 의류를 모두 구매해 본적이 있고 동시에 50만원 이상 구매 이력도 가진 고객들
+SELECT
+  c.customer_name,
+  c.customer_type
+FROM customers c
+WHERE 
+  -- 전자제품 구매
+  EXISTS(SELECT 1 FROM sales s1 WHERE s1.customer_id = c.customer_id AND s1.category = '전자제품')
+  AND
+  -- 의류 구매
+  EXISTS(SELECT 1 FROM sales s2 WHERE s2.customer_id = c.customer_id AND s2.category = '의류')
+  AND
+  -- 50만원 이상
+  EXISTS(SELECT 1 FROM sales s3 WHERE s3.customer_id = c.customer_id AND s3.total_amount >= 500000);
+  
+
+-- NOT EXISTS
+-- 판매 이력이 없는 상품들
+SELECT
+    p.product_name,
+    p.category,
+    p.selling_price
+FROM products p
+WHERE NOT EXISTS (
+    SELECT 1 FROM sales s WHERE s.product_name = p.product_name
+)
+ORDER BY p.category, p.selling_price DESC;
