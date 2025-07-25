848f5311f8576dc4b535cc5df0dba6473435ea54
noh000 <ay.noh89@gmail.com>
2025-07-25 15:50:05 +0900

    chatbot_day2

--- Python/telegram_chatbot/main.py
+++ Python/telegram_chatbot/main.py
@@ -25,17 +25,43 @@ uvicorn main(py 파일명):app(FastAPI 변수명) --reload
 Crtl+C
 '''
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 import random
+import requests
 
+bot_token = ''
+URL = f'https://api.telegram.org/bot{bot_token}'
 app =  FastAPI()
 
-@app.get('/hi')
-def hi():
-    return {'status': 'HI! :)'}
+@app.get('/')  # 라우팅  (목록 : http://127.0.0.1:8000/docs)(http://localhost:8000/docs)
+def home():
+    return {'home': 'sweet home'}
+
+
+# 라우팅으로 텔레그램 서버가 챗봇 업데이트가 있을 경우 알려줌
+
+def send_message(chat_id, message):
+    body = {
+        'chat_id': chat_id,
+        'text': message
+    }
+    requests.get(URL + '/sendMessage', body)
+
+@app.post('/telegram')
+async def telegram(request: Request):
+    print('noh-bot에 새로운 메세지가 들어왔습니다')
+
+    data = await request.json()    # 텔레그램이 보낸 데이터를 확인, 아직 이해 안 해도 됨
+    print(data)
+    
+    sender_id = data['message']['from']['id']
+    input_msg = data['message']['text']
+    send_message(sender_id, input_msg)
+    return {'status': 'message received'}
+
 
 @app.get('/lotto')
 def lotto():
     return {
-        'numbers' : sorted(random.sample(range(1, 46), 6))
+        'numbers': sorted(random.sample(range(1, 46), 6))
     }
